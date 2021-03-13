0103bc122ab2f2d9eb34664e26b5a62bae908301
asdf <asdf>
2021-03-13 15:42:36 -0600

    saving data

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"io/ioutil"
 	"math"
 	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/sgade/randomorg"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	loggable "github.com/sas1024/gorm-loggable"
 )
 
@@ -63,11 +65,118 @@ type Results struct {
 	loggable.LoggableModel
 }
 
+type LiveData []Dataset
+
+type Dataset struct {
+	ID         string      `json:"id"`
+	Subgroup   string      `json:"subgroup"`
+	SampleSize string      `json:"sampleSize"`
+	Population string      `json:"population"`
+	Grade      string      `json:"grade"`
+	URL        string      `json:"url"`
+	CreatedAt  string      `json:"created_at"`
+	StartDate  string      `json:"startDate"`
+	EndDate    string      `json:"endDate"`
+	Pollster   string      `json:"pollster"`
+	Answers    []Answers   `json:"answers"`
+	Type       string      `json:"type"`
+	SeatName   interface{} `json:"seat_name"`
+	Tracking   bool        `json:"tracking"`
+	HeadToHead bool        `json:"headToHead"`
+	InAvg      bool        `json:"inAvg"`
+	Cycle      string      `json:"cycle"`
+	State      string      `json:"state"`
+	Partisan   string      `json:"partisan,omitempty"`
+}
+type Answers struct {
+	Choice string `json:"choice"`
+	Pct    string `json:"pct"`
+	Party  string `json:"party"`
+}
+
 //Polls is the array of state polls.
 type Polls []State
 
 //SimType is the simulation Type used
 var SimType = CoinFlip
+
+func GetData() LiveData {
+
+	fmt.Println("Fetching Data")
+	var body []byte
+
+	var err error
+	if false {
+
+		resp, err := http.Get("https://projects.fivethirtyeight.com/polls/president-general/polls.json")
+		if err != nil {
+			// handle error
+		}
+		defer resp.Body.Close()
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		body, err = ioutil.ReadFile("./largeData.json")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	var data LiveData
+
+	fmt.Println("Marshaling Data")
+	if err = json.Unmarshal([]byte(body), &data); err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+func test() {
+	fmt.Println("Hello World")
+}
+func convertToData(data LiveData, sim Simulation) Simulation {
+
+	fmt.Printf("Recieved %d polls\n", len(data))
+	stateData := make(map[string]State)
+
+	for _, v := range data {
+
+		b := 0.0
+		t := 0.0
+		for _, v := range v.Answers {
+
+			d, err := strconv.ParseFloat(v.Pct, 64)
+			if err != nil {
+				continue
+			}
+			if strings.Contains(strings.ToLower(v.Choice), "trump") {
+				t = d
+				continue
+			}
+			if strings.Contains(strings.ToLower(v.Choice), "trump") {
+				t = d
+				continue
+			}
+		}
+
+		stateData[v.State] = State{
+			State: v.State,
+			Trump: t,
+			Biden: b,
+		}
+	}
+
+	sim.Polls = Polls{}
+	for i, v := range stateData {
+		fmt.Println("Adding State " + i)
+		sim.Polls = append(sim.Polls, v)
+	}
+	return sim
+
+}
 
 func main() {
 
@@ -80,6 +189,7 @@ func main() {
 	if err = json.Unmarshal([]byte(data), &sim); err != nil {
 		panic(err)
 	}
+	sim = convertToData(GetData(), sim)
 
 	var seed int64
 	if sim.Key != "" {
@@ -132,6 +242,8 @@ func (polls Polls) simulate(runs int) (trump, biden, tie float64) {
 	results := make(map[Candidate]float64)
 	var m sync.Mutex
 	var wg sync.WaitGroup
+
+	fmt.Println("Beginning Simulation")
 
 	wg.Add(runs)
 	for i := 0; i < runs; i++ {
@@ -224,6 +336,7 @@ func (s State) rawPollResults() (trump, biden int) {
 	return 0, 0
 }
 
+//flips a coin, where the winner is based on a random value, and if the percentage is past that value.
 func (s State) coinFlip() (trump, biden int) {
 
 	value := rand.Float64()
@@ -236,11 +349,13 @@ func (s State) coinFlip() (trump, biden int) {
 	return 0, s.Electors
 }
 
+//returns zero for both candidates
 func (s State) zeroes() (trump, biden int) {
 
 	return 0, 0
 }
 
+//using a bell curve, where the uncertanty is the std dev, randoms the winner.
 func (s State) bellCurve() (trump, biden int) {
 
 	dif := s.Variance * lookup(rand.Float64())
